eebaba16cd74a977dadc932f7f4ee705b84bed5c
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-06 20:42:24 +0800

    Added the html files
    Added all required html files to the templates folder. Also brushed up a bit of the tests code

--- Lab1/heroes/tests.py
+++ Lab1/heroes/tests.py
@@ -4,25 +4,26 @@ from django.test import TestCase
 class heroesTest(TestCase):
 
 	def test_heroes_link_returns_correct_html(self):
-		response = self.client.get('/')
+		response = self.client.get('/heroes')
 		self.assertTemplateUsed(response, 'home.html')
 
 
 class heroCloudTest(TestCase):
 
 	def test_hero_cloud_page_returns_correct_html(self):
-		response = self.client.get('/tasks')
+		response = self.client.get('/heroes/cloud')
 		self.assertTemplateUsed(response, 'task_list.html')
 
 
 class heroSunflowey(TestCase):
 
 	def test_hero_sunflowey_page_returns_correct_html(self):
-		response = self.client.get('/tasks/new')
+		response = self.client.get('/heroes/sunflowey')
 		self.assertTemplateUsed(response, 'task_create_form.html')
+		
 
 class heroJester(TestCase):
 
 	def test_hero_jester_page_returns_correct_html(self):
-		response = self.client.get('/tasks/new')
+		response = self.client.get('/heroes/jester')
 		self.assertTemplateUsed(response, 'task_create_form.html')# Create your tests here.
