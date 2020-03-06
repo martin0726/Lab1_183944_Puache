c016625534da75fc7b8d7b37c500e8b6a2970f0e
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-06 22:40:41 +0800

    Changed a few syntax errors

--- Lab1/heroes/urls.py
+++ Lab1/heroes/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import HeroesView, CloudView, SunfloweyView, JesterView
 
 urlpatterns = [
-	url(r'^heroes/$', HeroesView.as_view(), name='heroes_show'),
-	url(r'^hero/cloud$', CloudView.as_view(), name='cloud_show'),
-	url(r'^hero/sunflowey$', SunfloweyView.as_view(), name='sunflowey_show')
-	url(r'^heroes/jester$', JesterView.as_view(), name='jester_show')
+		url(r'^heroes/$', HeroesView.as_view(), name='heroes_show'),
+		url(r'^hero/cloud$', CloudView.as_view(), name='cloud_show'),
+		url(r'^hero/sunflowey$', SunfloweyView.as_view(), name='sunflowey_show')
+		url(r'^hero/jester$', JesterView.as_view(), name='jester_show')
 ]
