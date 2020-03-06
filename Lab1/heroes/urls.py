7eb764b03f448d5cba5280d7471917524eaa11cc
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-06 21:42:51 +0800

    Testing
    I tried to run the manage.py runserver. It asked me to migrate as well which I did

--- Lab1/heroes/urls.py
+++ Lab1/heroes/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url
 from .views import heroesView, cloudView, sunfloweyView, jesterView
 
 urlpatterns = [
-	url(r'^heroes/$', heroesView.as_view(), name='heroes'),
-	url(r'^heroes/cloud$', cloudView.as_view(), name='Cloud'),
-	url(r'^heroes/sunflowey$', sunfloweyView.as_view(), name='Sunflowey')
-	url(r'^heroes/jester$', jesterView.as_view(), name='Jester')
+	url(r'^heroes/$', heroesView.as_view(), name='heroes_show'),
+	url(r'^heroes/cloud$', cloudView.as_view(), name='cloud_show'),
+	url(r'^heroes/sunflowey$', sunfloweyView.as_view(), name='sunflowey_show')
+	url(r'^heroes/jester$', jesterView.as_view(), name='jester_show')
 ]
