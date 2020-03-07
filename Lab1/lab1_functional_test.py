0295d65cbf63b47dc9770383b9d62845957b031f
martin0726 <60959558+martin0726@users.noreply.github.com>
2020-03-07 16:50:25 +0800

    Filled up the functional tests
    I filled up the functional tests with the required functions. I also editted the html files by adding ids for the functional test to use

--- Lab1/lab1_functional_test.py
+++ Lab1/lab1_functional_test.py
@@ -11,7 +11,7 @@ class NewVisitorTest(unittest.TestCase):
 	def test_can_display_a_heroes_list_and_more_information_per_hero(self):
 		# Widget has heard about a new wiki app for the game called The Will of the Wisps. 
 		# She goes to check out its homepage
-		browser.get('http://localhost:8000')
+		self.browser.get('http://localhost:8000/heroes')
 
 		# She notices the page title and header mention 
 		# 'The Will of the Wisps Wiki'
@@ -19,13 +19,29 @@ class NewVisitorTest(unittest.TestCase):
 		
 		# She sees a list containing three heroes with their corresponding 
 		# names, health points, and damage 
+		self.assertIn(self.browser.get_current_url,
+			'http://localhost:8000/heroes')
 		
 		# When she selects one of the heroes, she is sent to another page
 		# containing more information about the hero (additional stats, lore, image).
-		
+		self.browser.get('http://localhost:8000/hero/cloud')
+
+		self.assertIn('Health Points', self.browser.find_element_by_id('hero_health').text)
+
+		self.assertIn('Base Attack Damage', self.browser.find_element_by_id('hero_attack').text)
+
+		self.assertIn('Skills', self.browser.find_element_by_id('hero_skills').text)	
+
+		self.assertIn('Lore', self.browser.find_element_by_id('hero_lore').text)
+
+		self.assertIn('.png,' self.browser.find_element_by_id('hero_image').get_attribute('src'))
 		# She spots the page title and header mentions the name of the hero she selected.
+		self.assertIn('Detail - ', self.browser.title)
 		
 		# While she is in a specific hero's page, she sees a button labeled "Back to Heroes List".
 		# She clicks this and she is redirected back to the wiki's homepage.	
 		
 		self.fail('Finish the test!')
+
+if __name__ == '__main__':
+	unittest.main(warnings = 'ignore')
